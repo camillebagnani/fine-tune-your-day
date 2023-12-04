f3bece8772bebe4c66ff5297ae9d9976d0ad4c28
camillebagnani <140850865+camillebagnani@users.noreply.github.com>
2023-12-04 16:59:24 -0700

    cleaned up planner.js and added close buttons

--- assets/planner.js
+++ assets/planner.js
@@ -1,4 +1,3 @@
-var plannerSection = $('#planner-section')
 var taskContainer = $('#task-container')
 var addTaskButton = $('#add-task')
 var submitButton = $('#submit')
@@ -11,9 +10,26 @@ var timeInput = $('#timeInput')
 var client_id = "4e26ad17c8bb4367873c37ff09d37cc6";
 var client_secret = "6c247c5fb58d420ca3243ca51bc0f947";
 
-// Input bar dropdown shows up once you click the 'Add Task' button
+// Close button attempt
+// var closeButton = $('.planner-section')
+
+// closeButton.click(function (event) {
+//     if($(event.target).hasClass("delete")) {
+//         // var currentTask;
+//         var currentCloseButton = $(event.target).siblings(".appendedTasks")
+//         currentCloseButton.closest(".appendedTasks").remove()
+
+//         // if($(event.target).hasClass("delete")) {
+//         //     // currentTask = $(event.target).parent().siblings(".appendedTasks").remove()
+//         //     currentCloseButton = $(event.target).siblings(".appendedTasks").remove()
+//         //     console.log(currentCloseButton)
+//         // }
+//     }
+// })
+
+// Form with task dropdown and time shows up once you click the 'Add Task' button
 addTaskButton.on('click', function () {
-    // Checks condition - if the input bar is hidden, it shows up and replaces the 'Add Task' button
+    // Checks condition - if the input bar is hidden, it shows up by replacing the 'Add Task' button
     if (form.hasClass("hidden")) {
         form.removeClass("hidden").addClass("show");
         addTaskButton.addClass("hidden").removeClass("show")
@@ -21,12 +37,14 @@ addTaskButton.on('click', function () {
     }
 })
 
+//Checks for local storage on load
 window.onload = function () {
     for (var i = 0; i < localStorage.length; i++) {
         var saved = JSON.parse(localStorage.getItem(i));
-        
+        // If there is local storage, it will grab local storage and append it the same way as the submitButton function
         if (localStorage !== null) {
-            taskContainer.append(`<div class="appendedTasks">
+            taskContainer.append(`<button class="delete"></button>
+            <div class="appendedTasks">
             <div>${(saved.Time)}</div>
             <div>${(saved.Task)}</div> 
             <div id="playlistRow">
@@ -35,12 +53,13 @@ window.onload = function () {
             <a href="${saved.PlaylistLink.Playlist3}" target="_blank"><div><img class="playlistImage" src="${saved.PlaylistImage.Image3}"></div></a>
             </div>
             </div>`)
-        
+
+            // The savedTasks array grabs the property values from the saved local storage if it exists
             var savedTasks = {
                 Task: saved.Task,
                 Time: saved.Time,
                 PlaylistLink: {
-                    Playlist1: saved.PlaylistLink.Playlist1, 
+                    Playlist1: saved.PlaylistLink.Playlist1,
                     Playlist2: saved.PlaylistLink.Playlist2,
                     Playlist3: saved.PlaylistLink.Playlist3,
                 },
@@ -50,24 +69,26 @@ window.onload = function () {
                     Image3: saved.PlaylistImage.Image3
                 }
             };
-            
+
             taskArray.push(savedTasks)
         }
     }
 }
 
+// Uses an async function to wait for the return of the promise from the Spotify API, then it can append the values from the Spotify API
 submitButton.on('click', async function () {
     var keyword = dropdown.val()
     var time = timeInput.val()
+
     if (dropdown.val() !== 'Select') {
-        // taskContainer.append(`<div>${keyword} ${time}</div>`)
-        // taskContainer.append(`<div>${time}</div>`)
         addTaskButton.removeClass("hidden").addClass("show") // Brings back the 'Add Task' button once 'keyword' variable is appended
         form.addClass("hidden").removeClass("show")
         resetForm()
         var playlist = await getSpotifyApi(keyword)
 
-        taskContainer.append(`<div class="appendedTasks">
+        // Appends a div that includes the user's selected time, task and playlists that were chosen by the keyword from the Spotify API call
+        taskContainer.append(`<button class="delete">X</button>
+        <div class="appendedTasks">
         <div>${time}</div>
         <div>${keyword}</div> 
             <div id="playlistRow">
@@ -75,13 +96,14 @@ submitButton.on('click', async function () {
             <a href="${playlist.playlistLinkArray[1]}" target="_blank"><div><img class="playlistImage" src="${playlist.playlistImageArray[1]}"></div></a>
             <a href="${playlist.playlistLinkArray[2]}" target="_blank"><div><img class="playlistImage" src="${playlist.playlistImageArray[2]}"></div></a>
             </div>
-            </div>`)
+            </div>`);
 
+        // Create an object of saved tasks to be pushed into taskArray, which we use to set local storage
         var savedTasks = {
             Task: keyword,
             Time: time,
             PlaylistLink: {
-                Playlist1: playlist.playlistLinkArray[0], 
+                Playlist1: playlist.playlistLinkArray[0],
                 Playlist2: playlist.playlistLinkArray[1],
                 Playlist3: playlist.playlistLinkArray[2]
             },
@@ -91,17 +113,16 @@ submitButton.on('click', async function () {
                 Image3: playlist.playlistImageArray[2]
             },
         };
-        
-        taskArray.push(savedTasks)
-        console.log(savedTasks)
-        
 
+        taskArray.push(savedTasks)
+        // Set local storage with the key as the index of the task array, and the value of the taskArray at index
         for (var i = 0; i < taskArray.length; i++) {
             localStorage.setItem(i, JSON.stringify(taskArray[i]));
         }
     }
 })
 
+// Resets the task and time inputs in the form
 function resetForm() {
     dropdown.val('Select');
     timeInput.val('');
@@ -150,125 +171,25 @@ async function getSpotifyApi(keyword) {
         }
     }
 
-
-    // async function main() {
     var access_token = await getAccessToken();
 
     var playlists = await getPlaylists(keyword, access_token);
 
-    // playlistContainer.html('');
-
+    // Create empty arrays so that we can return them at the end of the function
     var playlistLinkArray = [];
     var playlistImageArray = [];
 
     for (var i = 0; i < playlists.length; i++) {
         var playlistLink = playlists[i].external_urls.spotify;
         var playlistImage = playlists[i].images[0].url;
-        
+
+        // Push the values that the Spotify API call grabs to the playlistLinkArray and playlistImageArray so there will be values when we return
         playlistLinkArray.push(playlistLink);
         playlistImageArray.push(playlistImage);
-
-        // Append each playlist to the playlistContainer
-        // taskContainer.append(`<a href="${playlistLink}" target="_blank"><div><img src="${playlistImage}"></div></a>`);
     }
-    
+
     return {
         playlistLinkArray: playlistLinkArray,
         playlistImageArray: playlistImageArray
-      };
-    // return {playlistLink, playlistImage};
+    };
 }
-
-
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// }
-
-    // main();
-
-    
-
-
-    
-
-
-// function saveToStorage(keyword) {
-//     var savedTasks = JSON.parse(localStorage.getItem(taskArray.length)) || [];
-//     Object.values(savedTasks)
-//     savedTasks.push(keyword);
-//     localStorage.setItem("Tasks", JSON.stringify(taskArray))
-// }
-
-
-//localStorage.setItem("Tasks", JSON.stringify(taskArray))
-//taskArry.push({`"task":${taskName}, "time":{taskTime}, "playlist":${playlistFromAPICall}` })
-
-
-// need to be able to save and update local storage with the index of the id of the time I am changing
-
-// in save to storage function - it needs to know what index it needs to update
-// each key is going to be an index, the value of each one is going to be an object with the values of the time, task name and playlist links
-
-//which index im updating, what the key of the value im updating and what the new value is 
-// so when you refresh it will look at local storage and put them in the right places
-
-// If the dropdown value is anything other than "Select", append the changed value of dropdown and a time input
-// dropdown.change(function () {
-//     var keyword = dropdown.val()
-//     if (dropdown.val() !== 'Select') {
-//         tasks.append(`<div>${keyword}</div>`)
-//         taskArray.push(keyword)
-//         addTaskButton.removeClass("hidden").addClass("show") // Brings back the 'Add Task' button once 'keyword' variable is appended
-//         form.addClass("hidden").removeClass("show") // Hides the input bar when the button is back 
-//         getSpotifyApi(keyword)
-//         dropdown.val('Select') // Reverts the value of dropdown to 'Select' 
-//        // saveToStorage(keyword) // Saves the tasks to local storage
-//     }
-// })
-
-// taskContainer.on('change', timeInput, function (event) {
-    //     event.target = timeInput
-    //     keyword = timeInput.length
-    //     console.log(taskArray.length)
-    //     saveToStorage(keyword)
-    // })
-
-    // function renderSavedTasks(){
-//     var getItem = JSON.parse(localStorage.getItem("Saved Tasks"));
-//     console.log(getItem)
-//     if (getItem !== null) {
-//         for(var i = 0; i < getItem.length; i++) {
-//         storage.append(`<div>${getItem[i].Task}</div>`)
-//         storage.append(`<div>${getItem[i].Time}</div>`)
-//     }
-//     }
-
-//     console.log(getItem)
-//     // var retrievedTasks = JSON.parse(getItem);
-//     // taskContainer.textContent = getItem
-// }
-
-// renderSavedTasks()
-
-
-
-// console.log(retrievedTasks)
